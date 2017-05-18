1a535feef31d5c3d8190819bc36510aacd63c02c
Gary Mazzone Peters <gary.m.peters@vanderbilt.edu>
2017-05-18 11:15:24 -0500

    App running at basic capacity

--- app/src/main/java/com/example/andriod/cityguide/HousingActivity.java
+++ app/src/main/java/com/example/andriod/cityguide/HousingActivity.java
@@ -2,12 +2,10 @@ package com.example.andriod.cityguide;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
-/**
- * Created by GaryPeters on 5/17/17.
- */
 
 public class HousingActivity extends AppCompatActivity {
 
@@ -15,10 +13,26 @@ public class HousingActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.word_list);
+
+            ArrayList<Place> places = new ArrayList<>();
+
+            places.add(new Place("Crawford", "peabody", true, false, false, false, 4.0f));
+            places.add(new Place("East", "peabody", true, false, false, false, 2.0f));
+            places.add(new Place("West", "peabody", true, false, false, false, 1.5f));
+            places.add(new Place("North", "peabody", true, false, false, false, 2.5f));
+            places.add(new Place("Gillette", "peabody", true, false, false, false, 3.0f));
+
+
+            //set an adapter
+            PlaceAdapter itemsAdapter =
+                    new PlaceAdapter(this, places);
+
+            //set a listView
+            ListView listView = (ListView) findViewById(R.id.list);
+
+            //set adapter onto listview
+            listView.setAdapter(itemsAdapter);
+
         }
-
-        ArrayList<Place> places = new ArrayList<>();
-
-        places.add(new Place("Crawford", "peabody", true, false, false, false, 4));
 
 }
--- app/src/main/java/com/example/andriod/cityguide/PlaceAdapter.java
+++ app/src/main/java/com/example/andriod/cityguide/PlaceAdapter.java
@@ -1,19 +1,67 @@
 package com.example.andriod.cityguide;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by GaryPeters on 5/17/17.
  */
 
 public class PlaceAdapter extends ArrayAdapter<Place>{
+
+    public PlaceAdapter(Context context, ArrayList<Place> places){
+        super(context, 0, places);
+    }
+
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        //Get the data item for this position
+        Place place = getItem(position);
+
+        //To make it easier to think through
+        View listItemView = convertView;
+
+        //Check if an exisiting view is being reused, otherwise inflate the view
+        if (listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        //Lookup view for place data
+        TextView tvName = (TextView) listItemView.findViewById(R.id.name);
+        TextView tvRegion = (TextView) listItemView.findViewById(R.id.region);
+        TextView tvFresh = (TextView) listItemView.findViewById(R.id.freshman_tag);
+        TextView tvSoph = (TextView) listItemView.findViewById(R.id.sophomore_tag);
+        TextView tvJr = (TextView) listItemView.findViewById(R.id.junior_tag);
+        TextView tvSr = (TextView) listItemView.findViewById(R.id.senior_tag);
+        RatingBar rating = (RatingBar) listItemView.findViewById(R.id.ratingBar);
+
+        //Do Image secon
+
+        //Populate the data into the template view using the data object
+        //In this step where I have a textView defined and the Object defined im hooking each item up line by line
+        // so Java knows whats what, its essentially drawing lines to connect dots.
+        tvName.setText(place.getName());
+        tvRegion.setText(place.getRegion());
+
+        //Figure out what to do with these tags -- cause not just returning true false...
+        //tvFresh.setText(place.);
+        //tvSoph.setText(place.getName());
+        //tvJr.setText(place.getName());
+        //tvSr.setText(place.getName());
+
+        //Also should have set onClick Listners up first
+
+        return listItemView;
     }
 }
